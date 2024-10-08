45885e3a2ae48f811a1f8169afbf4351e6135409
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2024-10-08 16:22:52 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
-const maker = new SongMaker();
-maker.song.note("sine", 0, 1, 60, 0.5);
-maker.song.note("sine", 0, 1, 72, 0.5);
-const audioBlob = await maker.render("blob");
-const url = URL.createObjectURL(audioBlob);
-const audio = new Audio(url);
-audio.play();
-setTimeout(function() {
-	URL.revokeObjectURL(url);
-}, 1000)
+const songMaker = new SongMaker();
+songMaker.song.note("sine", 0, 1, 60, 0.5)
+songMaker.song.note("sine", 1, 2, 64, 0.5)
+songMaker.render().then(blob => {
+	const url = URL.createObjectURL(blob);
+	const audio = new Audio(url);
+	audio.play();
+	setTimeout(function() {
+		URL.revokeObjectURL(url);
+	}, 1000)
+})
